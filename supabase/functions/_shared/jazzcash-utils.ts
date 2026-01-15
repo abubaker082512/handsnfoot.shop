442d49e8a92d13beca7a725e1a9b778e47ddfa47
Abdullah Khalid <snakeyes358@gmail.com>
2026-01-16 01:38:56 +0500

    Fix CORS headers in edge functions

--- supabase/functions/_shared/jazzcash-utils.ts
+++ supabase/functions/_shared/jazzcash-utils.ts
@@ -71,6 +71,7 @@ export async function verifyHash(params, receivedHash, integritySalt) {
 export function corsHeaders() {
     return {
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
         'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
     };
 }
